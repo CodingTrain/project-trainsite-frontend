b9dc54480e6ba63aebbb5a9ac18e18aef9d74098
Fraasi <doofassi@gmail.com>
2019-01-06 13:26:00 +0200

    Header component, assets folder

---
+++ src/components/Header/index.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+// import { linkTo } from '@storybook/addon-links';
+
+import Header from '.';
+
+storiesOf('Header', module)
+  .add('header', () =>
+    <Header
+    />,
+  );
