cb68f99154f571790b2f4ccc92d7feecd944374b
Fraasi <doofassi@gmail.com>
2019-01-07 12:48:52 +0200

    fixed 'Consecutive blank lines are forbidden' & cleaned up variables.css

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './index';
 
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Header />, div);
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './index.css';
 
-
 export default class Header extends Component {
   render() {
 
