81b63d67ba4ea2e7cec883df1c7f8e88aeb4e547
Fraasi <doofassi@gmail.com>
2019-01-07 13:37:07 +0200

    fixed all the circleci linting errors

--- src/components/Footer/index.test.tsx
+++ src/components/Footer/index.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Footer from './index';
 
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Footer />, div);
