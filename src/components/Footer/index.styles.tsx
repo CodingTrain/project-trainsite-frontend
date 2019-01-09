a38cdbd1dd4d26939f882deff589916dfba020f9
Oliver Wright <meiamsome@meiamso.me>
2019-01-09 02:35:23 +0000

    Convert Footer to styled-components as a POC

---
+++ src/components/Footer/index.styles.tsx
@@ -0,0 +1,33 @@
+import styled from '../../helpers/style';
+
+export const FooterWrapper = styled.footer`
+  margin-top: 50px;
+  display: flex;
+  font-family: cubanoregular;
+
+  ${props => props.theme.media.tabletSmall`
+    flex-wrap: wrap;
+  `}
+`;
+
+export const Link = styled.a<{ color?: string, background: string }>`
+  padding: 40px;
+  flex-basis: 100%;
+  text-align: center;
+  text-decoration: none;
+  color: ${props => props.theme.colors[props.color || 'white']};
+  margin: 0;
+
+  span {
+    display: inline-block;
+    font-size: 18px;
+    transition: all 150ms ${props => props.theme.timingFunction};
+    transform: scale(1);
+  }
+
+  &:hover span {
+    transform: scale(1.2);
+  }
+
+  background: ${props => props.theme.colors[props.background]}
+`;
