b312e1160875eef960765885dbeecb888c123561
Fraasi <doofassi@gmail.com>
2019-01-17 11:20:00 +0200

    fixed test & storybook, cursed circleci

--- src/components/MobileNavigation/index.stories.tsx
+++ src/components/MobileNavigation/index.stories.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 // import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
-
+import { CodingTrainThemeProvider } from '../../helpers/style/theme';
+import { MemoryRouter as Router } from 'react-router-dom';
 import MobileNavigation from '.';
 
 storiesOf('MobileNavigation', module)
   .add('MobileNavigation', () =>
-    <MobileNavigation />,
+    <Router>
+      <CodingTrainThemeProvider>
+        You have to make the screen under 550px wide for this to show up.
+        <MobileNavigation />
+      </CodingTrainThemeProvider>
+    </Router>,
   );
--- src/components/MobileNavigation/index.test.tsx
+++ src/components/MobileNavigation/index.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MobileNavigation from './index';
+import { CodingTrainThemeProvider } from '../../helpers/style/theme';
+import { MemoryRouter as Router } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<MobileNavigation />, div);
+  ReactDOM.render(
+    <Router>
+      <CodingTrainThemeProvider>
+        <MobileNavigation />
+      </CodingTrainThemeProvider>
+    </Router>,
+    div);
   ReactDOM.unmountComponentAtNode(div);
 });
