95730e683ed7db5f0f34ad02d38dde33f5fdf7d3
Fraasi <doofassi@gmail.com>
2019-01-19 03:20:05 +0200

    rebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { CodingTrainThemeProvider } from './helpers/style/theme';
-import { PageWrapper } from './App.styles';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SupportTheCodingTrain from './components/SupportTheCodingTrain';
-import MobileNavigation from './components/MobileNavigation';
 import BackToTopButton from './components/BackToTopButton';
 import pic from './assets/images/octocat.png';
 
@@ -14,16 +12,12 @@ class App extends Component {
     return (
       <Router>
         <CodingTrainThemeProvider>
-          {/* PageWrapper is necessary for the blur effect */}
-          <PageWrapper id="page-wrapper">
             <Header />
             <img src={pic} style={{ height: '120px' }} /><br />
             <main>
               <Route exact path="/" component={SupportTheCodingTrain} />
             </main>
             <Footer />
-          </PageWrapper>
-          <MobileNavigation />
           <BackToTopButton />
         </CodingTrainThemeProvider>
       </Router>
