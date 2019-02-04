4cb70a49528946db8ca3c587cc0e625d8aeb37d0
Oliver Wright <meiamsome@meiamso.me>
2019-02-04 17:55:54 +0000

    Fix CSS linting

--- src/components/Navigation/index.style.tsx
+++ src/components/Navigation/index.style.tsx
@@ -2,6 +2,7 @@ import styled, { css } from '../../helpers/style';
 
 export const PageWrapper = styled.div<{ isOpen: boolean }>`
   transition: all 500ms ease-in-out;
-
-  ${props => props.isOpen && css`filter: blur(5px);`}
+  ${props => props.isOpen && css`
+    filter: blur(5px);
+  `}
 `;
